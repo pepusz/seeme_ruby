32b11f459716f3c5a2d34def7dcfb938d3275986
Rene Gabora <gabre71@gmail.com>
2016-06-16 08:32:03 +0200

    Handling errors on seems

--- lib/seeme/sms.rb
+++ lib/seeme/sms.rb
@@ -1,39 +1,41 @@
 module Seeme
   class Sms
-    TEMP = "tmp"
-    OUTBOUND = "outbound"
-    INBOUND = "inbound"
-    DRAFT = "draft"
-    SENT = "sent"
-    attr_reader :id, :from, :to, :text, :date, :type, :status, :delivery_date, :mock, :delivery, :message_id, :errors
-
-
-  
+    TEMP = 'tmp'
+    OUTBOUND = 'outbound'
+    INBOUND = 'inbound'
+    DRAFT = 'draft'
+    SENT = 'sent'
+    ERROR = 'failed'
+    attr_reader :id, :from, :to, :text, :date, :type, :status, :delivery_date,
+                :mock, :delivery, :message_id, :errors
 
     def initialize(options={})
       @errors = []
 
-      @from, @to, @text, @message_id = options[:from], options[:to], options[:text], options[:message_id]
+      @from, @to = options[:from], options[:to]
+      @text, @message_id = options[:text], options[:message_id]
       @type = TEMP
       @status = DRAFT
     end
 
     def send(client)
-      raise "Can't send because this message is already sent" if @type != TEMP  || @date.present? || is_sent?
-      raise "Can't send because to phone number not provided" if @to.blank?
-      raise "Can't send because from phone number not provided" if @from.blank?
-      raise "Can't send because message id not provided" if @message_id.blank?
-      
-      resp = client.get("", params)
-      
+      fail "Can't send because this message is already sent" if @type != TEMP  || @date.present? || is_sent?
+      fail "Can't send because to phone number not provided" if @to.blank?
+      fail "Can't send because from phone number not provided" if @from.blank?
+      fail "Can't send because message id not provided" if @message_id.blank?
+
+      resp = client.get('', params)
+
       resp_body = parse_response(resp)
-      if (resp_body[:result] == "OK" && resp.status == 200)
+
+      if (resp_body[:result] == 'OK')
         @date = Time.now
         @status = SENT
-        return true
+        true
+      else
+        @status = ERROR
+        false
       end
-
-      return false
     end
 
     def is_sent?
@@ -42,16 +44,16 @@ module Seeme
 
     private
     def params
-      {number: @to, sender: @from, message: @text, reference: @message_id}
+      { number: @to, sender: @from, message: @text, reference: @message_id }
     end
 
-     def parse_response(response)
+    def parse_response(response)
       resp_body = {}
       begin
         resp_body = MultiJson.load(response.body)
-        resp_body= resp_body.deep_symbolize_keys
-        if resp_body[:error].present?
-          @errors << resp_body[:error]
+        resp_body = resp_body.deep_symbolize_keys
+        if resp_body[:result] == 'ERR'
+          @errors <<  "#{resp_body[:code]} #{resp_body[:message]}"
         end
       rescue MultiJson::ParseError => exception
         @errors << exception.data
